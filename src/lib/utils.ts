df427ff91c06108b5025ccf4ab964d9a48d80b46
binikiya <biniyamkiya3682@gmail.com>
2024-10-09 14:56:45 +0300

    Full Commit

---
+++ src/lib/utils.ts
@@ -0,0 +1,27 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function timeAgo(dateString: string): string {
+  const date: Date = new Date(dateString);
+  const now: Date = new Date();
+  const diff: number = now.getTime() - date.getTime();
+  
+  const seconds: number = Math.floor(diff / 1000);
+  const minutes: number = Math.floor(seconds / 60);
+  const hours: number = Math.floor(minutes / 60);
+  const days: number = Math.floor(hours / 24);
+  
+  if (days > 1) {
+    return `${days} days ago`;
+  } else if (hours > 1) {
+    return `${hours} hours ago`;
+  } else if (minutes > 1) {
+    return `${minutes} minutes ago`;
+  } else {
+    return `${seconds} seconds ago`;
+  }
+}
